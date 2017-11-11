2726cf99cb68e0f53f031315dc1cb9373839d01d
karthik <karthik@automationtester.co.uk>
2017-11-11 21:23:52 +0000

    adding to github rep

---
+++ src/test/java/com/axone/samplemaven/LaunchChromeDriver.java
@@ -0,0 +1,13 @@
+package com.axone.samplemaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchChromeDriver {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+	}
+
+}
